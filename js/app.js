9120d9113c37647269980e7b71a2e24ba4af0738
amr88nzzal <amr.nzzal@gmail.com>
2021-03-16 20:56:23 +0200

    lab 03

--- js/app.js
+++ js/app.js
@@ -1,34 +1,58 @@
 'use strict';
+let userResult=0;
 let Age = document.getElementById('myAge');
 let home = document.getElementById('myhome');
 let useName = prompt('Please enter your name!');
 alert('Hello '+useName +'\n Its nice to see you in my page\n \n lets see if you know me!!!');
-let myAge = prompt('My age is greater than 27 years\n Yes/Y Or No/N?');
-console.log(myAge);
-let x=myAge==='yes';
-console.log(x);
-if(myAge.toLowerCase() === 'yes'||myAge.toLowerCase() === 'y')
+let myName = prompt('My name is Ammar \n Yes/Y Or No/N?');
+console.log(userResult);
+if(myName.toLowerCase() === 'yes'||myName.toLowerCase() === 'y')
 {
-  console.log(myAge);
-  alert ('you are right!!! \n My age will be shown in the page ');
-  Age.textContent = ('My age is 32 Years and your answer was correct ');
+  console.log(userResult);
+  alert ('you are wrong!!! \n My name is Amr ');
+}
+else if(myName.toLowerCase() === 'no'||myName.toLowerCase() === 'n')
+{
+  userResult++;
+  console.log(userResult);
+  alert ('you are right!!! \n My name is Amr');
 }
 else
 {
-  console.log(myAge);
+  alert ('wrong input!!!');
+}
+
+let myAge = prompt('My age is greater than 27 years\n Yes/Y Or No/N?');
+console.log(userResult);
+if(myAge.toLowerCase() === 'yes'||myAge.toLowerCase() === 'y')
+{
+  userResult++;
+  console.log(userResult);
+  alert ('you are right!!! \n My age will be shown in the page ');
+  Age.textContent = ('My age is 32 Years and your answer was correct ');
+}
+else if(myAge.toLowerCase() === 'no'||myAge.toLowerCase() === 'n')
+{
+  console.log(userResult);
   alert ('you are wrong!!! \n My age will be shown in the page ');
   Age.textContent = 'My age is 32 Years and your answer was wrong !!';
 }
+else
+{
+  alert ('wrong input!!!');
+}
+
 let myHome = prompt('Iam from Syria \n Yes/Y Or No/N?');
 if(myHome.toLowerCase() === 'yes'||myHome.toLowerCase() === 'y')
 {
-  console.log(home);
+  userResult++;
+  console.log(userResult);
   alert ('you are right!!! \n Great job ');
   home.textContent = ('I am from Syria and your answer was correct ');
 }
 else
 {
-  console.log(home);
+  console.log(userResult);
   alert ('you are wrong!!! \n sorry ');
   home.textContent = ('I am from  Syria and your answer was wrong !!');
 }
@@ -37,35 +61,101 @@ switch(myStudy.toLowerCase())
 {
 case 'yes':
 case 'y':
-  console.log(myStudy);
+  console.log(userResult);
   alert ('OPSSSSS!!! \n That was wrong!! \n I studied Accounting ');
   break;
 case 'no':
 case 'n':
-  console.log(myStudy);
+  userResult++;
+  console.log(userResult);
   alert ('perfect!!! \nI studied Accounting');
   break;
 default :
-  console.log(myStudy);
+  console.log(userResult);
   alert ('wrong input!!!');
   break;
 }
 
-let weMet = prompt('Did we met before?? \n Yes/Y Or No/N?');
-switch(weMet.toLowerCase())
+let carColor = prompt('My car\'s color is Red \n Yes/Y Or No/N?');
+switch(carColor.toLowerCase())
 {
 case 'yes':
 case 'y':
-  console.log(weMet);
-  alert ('It was great chance to know you 😊');
+  console.log(userResult);
+  alert ('Sorry , You are wrong \n It is Dark Gray');
   break;
 case 'no':
 case 'n':
-  console.log(weMet);
-  alert ('It will be a pleasure to meet you soon 😊');
+  userResult++;
+  console.log(userResult);
+  alert ('You are doing fine !!');
   break;
 default :
-  console.log(weMet);
+  console.log(userResult);
   alert ('wrong input!!!');
   break;
 }
+
+/*********************************lab 03**************************************/
+let randNum = Math.floor(Math.random() * 100) + 1;
+let gNum = prompt('lets play a game \nI picked a random number between(0-100) and you should guess it\nYou have just 4 attempts\nI will help you\n'+(randNum+1));
+let gResult =0;
+for(let i =0;i<4;i++)
+
+{
+  if(Number(gNum) === randNum)
+  {
+    gResult=1;
+    userResult++;
+    console.log(userResult);
+    break;
+  }
+  else if (gNum < randNum && i!==3)
+  {
+    gNum = prompt('Wrong!!\nYou should choose a higher number!  \n\n you have '+(3-i)+' Attempt');
+  }
+  else if (gNum > randNum && i!==3)
+  {
+    gNum = prompt('Wrong!!\nYou should choose a smaller number! \n\n you have '+(3-i)+' Attempt');
+  }
+}
+if(gResult===0)
+{
+  alert('Sorry !!! \n Your 4 answers were wrong!! \n\n\n the answer is : '+randNum);
+}
+else
+{
+  {alert('Great Job !!! \n You Could find the answer!!');}
+}
+/* ***************************************************/
+let progLang = ['python','javascript','java','c#','c','c++','go','flutter','php','dart'];
+let gProgLang=prompt('Could you give me a name of one of top 10 programing languages??\nYou have 6 attempt ');
+let gAns=0;
+for(let a=0;a<6;a++)
+{
+  for(let i=0;i<progLang.length;i++)
+  {
+    if(gProgLang.toLowerCase()===progLang[i])
+    {
+      alert('perfect!!\n (('+ gProgLang.toUpperCase()+')) Is Correct Answer');
+      userResult++;
+      console.log(userResult);
+      i=progLang.length;
+      a=7;
+      gAns=1;
+    }
+  }
+  if(gAns<1)
+  {if(a<5){
+    gProgLang=prompt('Sorry, Your answer in not in my list \nYou have '+(5-a)+' attempt ');
+  }}
+
+}
+let allLang='';
+for(let i=0;i<progLang.length;i++){
+  allLang=allLang+'\n'+progLang[i].toUpperCase();
+
+}
+alert ('Top 10 Languages are :'+allLang);
+
+alert('Your correct answers are :\n'+userResult +' out of 7!!');
