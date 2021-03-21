30d3ab0cb9799cd5f59d89d20d097ea3cac4d055
amr88nzzal <amr.nzzal@gmail.com>
2021-03-21 23:40:31 +0200

    update

--- js/app.js
+++ js/app.js
@@ -6,37 +6,30 @@ let useName = prompt('Please enter your name!');
 alert('Hello '+useName +'\n Its nice to see you in my page\n \n lets see if you know me!!!');
 function q1(){
   let myName = prompt('My name is Ammar \n Yes/Y Or No/N?');
-  console.log(userResult);
   if(myName.toLowerCase() === 'yes'||myName.toLowerCase() === 'y')
   {
-    console.log(userResult);
     alert ('you are wrong!!! \n My name is Amr ');
   }
   else if(myName.toLowerCase() === 'no'||myName.toLowerCase() === 'n')
   {
     userResult++;
-    console.log(userResult);
     alert ('you are right!!! \n My name is Amr');
   }
   else
   {
     alert ('wrong input!!!');
   }
-
 }
 function q2(){
   let myAge = prompt('My age is greater than 27 years\n Yes/Y Or No/N?');
-  console.log(userResult);
   if(myAge.toLowerCase() === 'yes'||myAge.toLowerCase() === 'y')
   {
     userResult++;
-    console.log(userResult);
     alert ('you are right!!! \n My age will be shown in the page ');
     Age.textContent = ('My age is 32 Years and your answer was correct ');
   }
   else if(myAge.toLowerCase() === 'no'||myAge.toLowerCase() === 'n')
   {
-    console.log(userResult);
     alert ('you are wrong!!! \n My age will be shown in the page ');
     Age.textContent = 'My age is 32 Years and your answer was wrong !!';
   }
@@ -50,13 +43,11 @@ function q3(){
   if(myHome.toLowerCase() === 'yes'||myHome.toLowerCase() === 'y')
   {
     userResult++;
-    console.log(userResult);
     alert ('you are right!!! \n Great job ');
     home.textContent = ('I am from Syria and your answer was correct ');
   }
   else
   {
-    console.log(userResult);
     alert ('you are wrong!!! \n sorry ');
     home.textContent = ('I am from  Syria and your answer was wrong !!');
   }
@@ -67,17 +58,14 @@ function q4(){
   {
   case 'yes':
   case 'y':
-    console.log(userResult);
     alert ('OPSSSSS!!! \n That was wrong!! \n I studied Accounting ');
     break;
   case 'no':
   case 'n':
     userResult++;
-    console.log(userResult);
     alert ('perfect!!! \nI studied Accounting');
     break;
   default :
-    console.log(userResult);
     alert ('wrong input!!!');
     break;
   }
@@ -88,17 +76,14 @@ function q5(){
   {
   case 'yes':
   case 'y':
-    console.log(userResult);
     alert ('Sorry , You are wrong \n It is Dark Gray');
     break;
   case 'no':
   case 'n':
     userResult++;
-    console.log(userResult);
     alert ('You are doing fine !!');
     break;
   default :
-    console.log(userResult);
     alert ('wrong input!!!');
     break;
   }
@@ -115,7 +100,6 @@ function q6(){
     {
       gResult=1;
       userResult++;
-      console.log(userResult);
       break;
     }
     else if (gNum < randNum && i!==3)
@@ -142,32 +126,25 @@ function q7(){
   let gProgLang=prompt('Could you give me a name of one of top 10 programing languages??\nYou have 6 attempt ');
   let gAns=0;
   for(let a=0;a<6;a++)
-  {
-    for(let i=0;i<progLang.length;i++)
-    {
-      if(gProgLang.toLowerCase()===progLang[i])
-      {
-        alert('perfect!!\n (('+ gProgLang.toUpperCase()+')) Is Correct Answer');
-        userResult++;
-        console.log(userResult);
-        i=progLang.length;
-        a=7;
-        gAns=1;
-      }
-    }
-    if(gAns<1)
-    {if(a<5){
-      gProgLang=prompt('Sorry, Your answer in not in my list \nYou have '+(5-a)+' attempt ');
-    }}
-
+  {for(let i=0;i<progLang.length;i++)
+  {if(gProgLang.toLowerCase()===progLang[i])
+  { alert('perfect!!\n (('+ gProgLang.toUpperCase()+')) Is Correct Answer');
+    userResult++;
+    i=progLang.length;
+    a=7;
+    gAns=1;
+  }
+  }
+  if(gAns<1)
+  {if(a<5){
+    gProgLang=prompt('Sorry, Your answer in not in my list \nYou have '+(5-a)+' attempt ');
+  }}
   }
   let allLang='';
   for(let i=0;i<progLang.length;i++){
     allLang=allLang+'\n'+progLang[i].toUpperCase();
-
   }
   alert ('Top 10 Languages are :'+allLang);
-
   alert('Your correct answers are :\n'+userResult +' out of 7!!');
 }
 q1();
